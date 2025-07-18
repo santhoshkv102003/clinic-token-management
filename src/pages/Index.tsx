b47b7013ff0a31a1b5c08e03e0df73dbd8195ba8
santhoshkv102003 <kvsanthosh1011@gmail.com>
2025-07-18 09:03:39 +0530

    image add

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -116,14 +116,7 @@ const Index = () => {
   };
 
   return (
-    <div
-      className="min-h-screen"
-      style={{
-        backgroundImage: "url('/stethoscope-bg.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <div className="min-h-screen bg-background">
       {/* Header */}
       <header className="border-b bg-card/50 backdrop-blur-sm sticky top-0 z-50">
         <div className="container mx-auto px-4 py-4">
